5cfc7ce970dc4b6f5735dbcdf3d4c0b03e61e88f
Lint Action <lint-action@samuelmeuli.com>
2022-02-17 08:19:34 +0000

    Fix code style issues with Prettier

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -5,5 +5,5 @@ import { Token } from './token'
  */
 export const WETH9: { [chainId: number]: Token } = {
   [1]: new Token(1, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18, 'WETH', 'Wrapped Ether'),
-  [3]: new Token(3, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether'),
+  [3]: new Token(3, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether')
 }
