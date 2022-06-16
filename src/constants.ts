20ef54c6b68334b00b11da707da98fca3bad2403
NakaNaoTest <naobook7@gmail.com>
2022-06-16 22:27:10 +0900

    change factory address and init code hash

--- src/constants.ts
+++ src/constants.ts
@@ -16,9 +16,9 @@ export enum Rounding {
 
 export const MaxUint256 = JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
 
-export const FACTORY_ADDRESS = '0x5f040BffAC55cb29cbB98593dc684732Fe035557'
+export const FACTORY_ADDRESS = '0x193950159f7b3b23bBfe49ff90fD2B3b0aA1E72c'
 
-export const INIT_CODE_HASH = '0x5584839d90031fc861236f3668a716a7c1c7a83a670ba1778c113b2a399eb803'
+export const INIT_CODE_HASH = '0x49bc0a6067f7b18489ee392419f6131ff6c252a51bea1ee17d2e0f34ffd96c28'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
